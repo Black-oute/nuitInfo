b1a82991f7fb4094151248edeff319d147a30978
EwanGrave <68923027+EwanGrave@users.noreply.github.com>
2021-12-02 18:49:59 +0100

    form connexion

---
+++ html/connection.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Connexion</title>
+</head>
+
+<body>
+
+    <div id="main-content">
+
+        <form>
+            <div class="mb-3">
+                <label for="email" class="form-label">Adresse email</label>
+                <input type="email" class="form-control" id="email">
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Mot de passe</label>
+                <input type="password" class="form-control" id="password">
+            </div>
+            
+            <button type="submit" class="btn btn-primary">Valider</button>
+        </form>
+
+    </div>
+
+</body>
+
+</html>
