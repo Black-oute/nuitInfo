0c9f97cdd882e80961e56c8431ff51b1d81cdb22
EwanGrave <68923027+EwanGrave@users.noreply.github.com>
2021-12-02 20:54:12 +0100

    feat: requête de connexion de l'user

--- models/connection.php
+++ models/connection.php
@@ -1,8 +1,31 @@
 <?php
 
+require_once "connectionPDO.php";
+
 if (isset($_POST["email"]) && !empty($_POST["email"]) && isset($_POST["password"]) && !empty($_POST["password"]))
 {
     // requête sql de recherhe de l'user
+    $stmt = $db->prepare(<<<SQL
+    SELECT *
+    FROM Utilisateur
+    WHERE email = :email
+    AND password = :password
+    SQL);
+
+    $stmt->execute(array(":email" => $_POST["email"], ":password" => hash("sha256", $_POST["password"])));
+
+    if ($stmt->rowCount() == 1)
+    {
+        $stmt = $stmt->fetch();
+        $_SESSION["username"] = $stmt["pseudo"];
+        $_SESSION["email"] = $stmt["email"];
+        $_SESSION["firstname"] = $stmt["prenom"];
+        $_SESSION["lastname"] = $stmt["nom"];
+    }
+    else
+    {
+        // ERREUR
+    }
 }
 else
 {
